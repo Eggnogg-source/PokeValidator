c403c8101bc0897bc38abb012fed9f7b03581f59
Eggnog <ChristianMGomez@outlook.com>
2025-11-25 14:57:25 -0500

    Update catch-all route handler for Vercel

--- api/[...].js
+++ api/[...].js
@@ -1,4 +1,8 @@
 // Catch-all route for Vercel serverless functions
-// This file handles all /api/* routes
-module.exports = require('./index.js');
+// This file handles all routes (both /api/* and /*)
+const app = require('./index.js');
+
+// Export as Vercel serverless function handler
+module.exports = app;
+module.exports.default = app;
 
